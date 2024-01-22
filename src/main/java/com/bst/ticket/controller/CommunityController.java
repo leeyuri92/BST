f902ddaf18011c7753a314b7534617b2dfc264f7
subeenjeon <145312273+nasubeeni120@users.noreply.github.com>
2024-01-22 19:24:48 +0900

    Add feature: Community board

--- src/main/java/com/bst/ticket/controller/CommunityController.java
+++ src/main/java/com/bst/ticket/controller/CommunityController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bst.ticket.service.CommunityService;
@@ -11,10 +13,19 @@ import com.bst.ticket.service.CommunityService;
 @Controller
 @RequestMapping("/community/*")
 public class CommunityController {
-  Logger logger = LoggerFactory.getLogger(CommunityController.class);
+    Logger logger = LoggerFactory.getLogger(CommunityController.class);
 
-  @Autowired
-  private CommunityService communityService;
+    @Autowired
+    private CommunityService communityService;
 
-    
+  /*
+  Author: 전수빈
+  Data: 22 Jan
+  Feature: Community board
+   */
+    @GetMapping("/")
+    public String communityPage() {
+        return "community";
+    }
+
 }
