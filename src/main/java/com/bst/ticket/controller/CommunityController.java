94acc3d66451e85ae261076ec773ea3b1975ba98
subeenjeon <145312273+nasubeeni120@users.noreply.github.com>
2024-01-29 15:14:19 +0900

    fix: comment

--- src/main/java/com/bst/ticket/controller/CommunityController.java
+++ src/main/java/com/bst/ticket/controller/CommunityController.java
@@ -8,14 +8,10 @@ import com.bst.ticket.vo.CommunityVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.bst.ticket.service.CommunityService;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
